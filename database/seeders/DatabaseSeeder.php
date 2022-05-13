32a17c94534cb5ae43eed154da223ba76ddda6d0
alex-killer <onefrank@bk.ru>
2022-05-13 16:28:59 +0700

    1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +16,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         User::factory(10)->create();
+        User::truncate();
+        Post::truncate();
+        Category::truncate();
+
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal',
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family',
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work',
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-family-post',
+            'excerpt' => '<p>Lorem ipsum dolor sit amet, consectetur</p>',
+            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, expedita harum hic neque qui recusandae repellat repudiandae! Architecto dolorem earum esse illum, impedit obcaecati perferendis quia quos sit soluta tempore.</p>',
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => '<p>Lorem ipsum dolor sit amet, consectetur</p>',
+            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, expedita harum hic neque qui recusandae repellat repudiandae! Architecto dolorem earum esse illum, impedit obcaecati perferendis quia quos sit soluta tempore.</p>',
+        ]);
     }
 }
